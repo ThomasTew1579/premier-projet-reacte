95cfb684bf6ad166b1465a0622d4f26252e8f59b
Thomas <pasquet.thomas69@gmail.com>
2022-10-19 11:59:05 +0200

    Updates

---
+++ precache-manifest.b6c2debef198f28f1b4dd72803fc9dbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15abbbbd58f62cf27e206c2361bbccaf",
+    "url": "/premier-projet-reacte/index.html"
+  },
+  {
+    "revision": "56ec86d8451384fd64a3",
+    "url": "/premier-projet-reacte/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "52ca50e49cbea25d187d",
+    "url": "/premier-projet-reacte/static/css/main.790a52af.chunk.css"
+  },
+  {
+    "revision": "56ec86d8451384fd64a3",
+    "url": "/premier-projet-reacte/static/js/2.92a4fd71.chunk.js"
+  },
+  {
+    "revision": "52ca50e49cbea25d187d",
+    "url": "/premier-projet-reacte/static/js/main.20ed96ad.chunk.js"
+  },
+  {
+    "revision": "812fbb3bc1b5ca86bcb9",
+    "url": "/premier-projet-reacte/static/js/runtime~main.a47de5c2.js"
+  }
+]);
